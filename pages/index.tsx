5ffb803465cefc0309e47fd0403e7a2764c60a60
Ayberk Uman <ayberkuman1@gmail.com>
2023-07-14 16:30:52 +0300

    index file refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,16 @@
-import React from "react";
-import { SetStateAction, useState } from "react";
-
 import Head from "next/head";
 
-import iconLinkedIn from "../assets/iconLinkedIn.png";
-import iconGitHub from "../assets/iconGitHub.png";
-import iconArtStation from "../assets/iconArtStation.png";
-import iconYouTube from "../assets/iconYouTube.png";
-import iconTwitter from "../assets/iconTwitter.png";
-import iconInstagram from "../assets/iconInstagram.png";
-
+import Customization from "@/components/Customization";
+import Info from "@/components/Info";
 import Step from "@/components/Step";
 import Template from "@/components/Template";
-import Info from "@/components/Info";
-import Customization from "@/components/Customization";
-
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-const templates = [
-  "Plain Text",
-  "Template 1 Left",
-  "Template 1 Right",
-  "Template 2 Left",
-  "Template 2 Right",
-  "Template 3 Top",
-  "Template 3 Bottom",
-];
-const parts = [1, 2, 3];
+import { useStepStore } from "@/store/stepStore";
+import { useTemplateStore } from "@/store/templateStore";
+import { Preview } from "@/components/Preview";
 
 export default function Home() {
-  const [step, setStep] = useState(1);
-  const [template, setTemplate] = useState("");
-
-  const handleSetStep = (selectedStep: SetStateAction<number>) => {
-    setStep(selectedStep);
-  };
-
-  const handleTemplateChange = (selectedTemplate: SetStateAction<string>) => {
-    setTemplate(selectedTemplate);
-  };
-
+  const { step } = useStepStore();
+  const { template } = useTemplateStore();
   return (
     <>
       <Head>
@@ -61,7 +30,12 @@ export default function Home() {
         </main>
 
         <aside className="hidden sticky shrink-0 h-screen top-8 w-96 xl:block bg-window">
-          {/* Preview Component */}
+          <Preview
+            outerDiv={template.id}
+            profileIcon={template.id}
+            userInfo={template.id}
+            linkIcons={template.id}
+          />
         </aside>
       </div>
     </>
