6f95a7f98378e619fa4c163706f1ce2f391bb480
venrig <egeoruc26@gmail.com>
2023-07-14 16:23:17 +0300

    index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { SetStateAction, useState } from "react";
 
-import Image from "next/image";
+import Head from "next/head";
 
 import iconLinkedIn from "../assets/iconLinkedIn.png";
 import iconGitHub from "../assets/iconGitHub.png";
@@ -11,61 +11,64 @@ import iconTwitter from "../assets/iconTwitter.png";
 import iconInstagram from "../assets/iconInstagram.png";
 
 import Step from "@/components/Step";
-import TemplateTitle from "@/components/Heading";
 import Template from "@/components/Template";
 import Info from "@/components/Info";
 import Customization from "@/components/Customization";
 
 import { Inter } from "next/font/google";
-import Heading from "@/components/Heading";
-import { useStepStore } from "@/store/stepStore";
-import { Preview } from "@/components/Preview";
-import { useTemplateStore } from "@/store/templateStore";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const templates = [
+  "Plain Text",
+  "Template 1 Left",
+  "Template 1 Right",
+  "Template 2 Left",
+  "Template 2 Right",
+  "Template 3 Top",
+  "Template 3 Bottom",
+];
+const parts = [1, 2, 3];
+
 export default function Home() {
-  const { step } = useStepStore();
-  const { template } = useTemplateStore();
+  const [step, setStep] = useState(1);
+  const [template, setTemplate] = useState("");
+
+  const handleSetStep = (selectedStep: SetStateAction<number>) => {
+    setStep(selectedStep);
+  };
+
+  const handleTemplateChange = (selectedTemplate: SetStateAction<string>) => {
+    setTemplate(selectedTemplate);
+  };
 
   return (
-    <div
-      className="flex w-full max-w-[1440px] mx-auto px-4 py-10 sm:px-6 lg:px-8 gap-x-8 items-start
-    bg-background"
-    >
-      <aside
-        className="hidden sticky shrink-0 h-screen top-8
-      w-44 lg:block
-      bg-window overflow-wrap "
-      >
-        {/* Left column area */}
-        <Step />
-      </aside>
+    <>
+      <Head>
+        <title>Email Signature Generator - Nureply</title>
+      </Head>
 
-      <main
-        className="flex-1
-      bg-window"
-      >
-        {step === 1 && <Template />}
-        {/*  {step === 2 && (
-          <Info templateType={template} handleTemplate={handleTemplateChange} />
-        )} */}
-        {step === 3 && <div>{/* Style Changer Component */}</div>}
-      </main>
+      <div className="flex w-full max-w-[1440px] mx-auto px-4 py-10 sm:px-6 lg:px-8 gap-x-8 items-start bg-background">
+        <aside className="hidden sticky shrink-0 h-screen top-8 w-44 lg:block bg-window overflow-wrap">
+          {/* Left column area */}
+          <Step setStep={handleSetStep} step={step} />
+        </aside>
 
-      <aside
-        className="hidden sticky shrink-0 h-screen top-8
-      w-96 xl:block
-      bg-window"
-      >
-        {/* Preview Component */}
-        <Preview
-          outerDiv={template.id}
-          profileIcon={template.id}
-          linkIcons={template.id}
-          userInfo={template.id}
-        />
-      </aside>
-    </div>
+        <main className="flex-1 bg-window">
+          {step === 1 && (
+            <Template
+              templateType={template}
+              handleTemplate={handleTemplateChange}
+            />
+          )}
+          {step === 2 && <Info />}
+          {step === 3 && <Customization />}
+        </main>
+
+        <aside className="hidden sticky shrink-0 h-screen top-8 w-96 xl:block bg-window">
+          {/* Preview Component */}
+        </aside>
+      </div>
+    </>
   );
 }
