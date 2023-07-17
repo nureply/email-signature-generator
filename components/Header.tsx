dd007f0e310e191b4b180e9b33611f06f94ffd9d
Onur Doğan <crrccnt@pm.me>
2023-07-17 16:23:55 +0300

    added icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Image from "next/image";
+
+import iconNureply from "../public/favicon.ico";
 
 const Header = () => {
     return (
         <header className="flex p-8 justify-between items-center bg-window fixed top-0 left-0 right-0 z-50">
+            <div className="items-center flex overflow-hidden">
+                <Image src={iconNureply} width={80} height={80} alt="" />
             <h1 className="text-2xl text-default">
                 Email Signature Generator - Nureply
             </h1>
+            </div>
+
+
             <button className="px-8 py-2 rounded bg-background cursor-not-allowed" disabled>
                 Generate
             </button>
