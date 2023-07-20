1699f0e166853424fd3fb0613af0e1c226d9d4ca
Onur Doğan <crrccnt@pm.me>
2023-07-20 17:02:37 +0300

    with preview

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,28 +1,31 @@
-import isEmail from "validator/lib/isEmail";
+import { useForm } from 'react-hook-form';
 
-export function validateEmail(email: string): boolean {
-  return isEmail(email);
+function isEmailValid(email: string): boolean {
+  // Implement a simple email validation logic here
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  return email === '' || emailRegex.test(email);
 }
 
-export function validateLink(link: string): boolean {
+function isLinkValid(link: string): boolean {
   try {
+    // Implement your link validation logic here
     const url = new URL(link);
     const hostname = url.hostname;
     const pathname = url.pathname;
 
     switch (true) {
-      case hostname.includes("linkedin.com") && pathname.startsWith("/in/"):
+      case hostname.includes('linkedin.com') && pathname.startsWith('/in/'):
         return true;
-      case hostname.includes("github.com") && pathname.split("/").length === 2:
+      case hostname.includes('github.com') && pathname.split('/').length === 2:
         return true;
-      case hostname.includes("youtube.com") && pathname.startsWith("/user/"):
+      case hostname.includes('youtube.com') && pathname.startsWith('/user/'):
         return true;
-      case hostname.includes("twitter.com") && pathname.split("/").length === 2:
+      case hostname.includes('twitter.com') && pathname.split('/').length === 2:
         return true;
-      case hostname.includes("facebook.com") && pathname.startsWith("/"):
+      case hostname.includes('facebook.com') && pathname.startsWith('/'):
         return true;
-      case hostname.includes("instagram.com") &&
-        pathname.split("/").length === 2:
+      case hostname.includes('instagram.com') &&
+        pathname.split('/').length === 2:
         return true;
       default:
         return false;
@@ -30,4 +33,25 @@ export function validateLink(link: string): boolean {
   } catch (e) {
     return false;
   }
+}
+
+export function useValidation() {
+  const { register, watch } = useForm({
+    defaultValues: {
+      email: '',
+      link: '',
+    },
+  });
+
+  const watchedEmail = watch('email', '');
+  const watchedLink = watch('link', '');
+
+  const isEmailValidResult = isEmailValid(watchedEmail);
+  const isLinkValidResult = isLinkValid(watchedLink);
+
+  return {
+    register,
+    isEmailValid: isEmailValidResult,
+    isLinkValid: isLinkValidResult,
+  };
 }
