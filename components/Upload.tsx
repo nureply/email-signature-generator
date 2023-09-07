7c237c4b77e481d504927dd18fd1937134a749f8
Hasan Uğur <138374975+hasanugurertem@users.noreply.github.com>
2023-09-07 17:53:09 +0300

    info sayfası

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -4,7 +4,7 @@ import useInfoStore from '../store/infoStore';
 const Upload = () => {
     const [openAccordion, setOpenAccordion] = useState(null);
 
-    const toggleAccordion = (index) => {
+    const toggleAccordion = (index:any) => {
         if (openAccordion === index) {
             setOpenAccordion(null);
         } else {
@@ -19,26 +19,30 @@ const Upload = () => {
     return (
         <>
             <div className="m-6">
-                <h1 className="not-italic text-6xl text-center font-bold mb-4">Free email signature generator</h1>
-                <h1 className="not-italic text-xl text-center font-bold mb-4">Get a professional email signature that won’t send your emails to spam</h1>
-                <br />
+                <div className='py-20'>
+                <h1 className="not-italic text-6xl text-center font-bold mb-4 ">Free email signature generator</h1>
+                <p className="not-italic text-xl text-center mb-4">Get a professional email signature that won’t send your emails to spam</p>
+                
+                </div>
+               
+                
                 
 
-                <div className="min-h-screen bg-white mx-40">
-                <h1 className="not-italic text-3xl font-bold mb-4">How to make an email signature?</h1>
+                <div className="min-h-screen bg-white mx-auto max-w-3xl ">
+                <h1 className=" text-2xl font-semibold mb-4">How to make an email signature?</h1>
                     <div onClick={() => toggleAccordion(1)} className="p-4 bg-white w-full rounded flex justify-between items-center cursor-pointer">
                         <div className="flex items-center gap-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" className={`h-10 w-10 ml-2 ${openAccordion === 1 ? 'rotate-180' : ''}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className={`h-8 w-8 ml-2 ${openAccordion === 1 ? 'rotate-180' : ''}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
                             </svg>
-                            <h4 className="text-3xl text-black">
+                            <h4 className="text-xl text-black ">
                                 Choose a template
                             </h4>
                         </div>
                     </div>
                     {openAccordion === 1 && (
                         <div className="w-full bg-white p-4">
-                            <h4 className="text-2xl text-black ml-14">Pick a template and fill it with your personal &amp; professional details.</h4>
+                            <h4 className="text-lg text-gray-900 ml-14">Pick a template and fill it with your personal &amp; professional details.</h4>
                         </div>
                     )}
                     <hr className="border-b-1 border-black" />
@@ -104,8 +108,10 @@ const Upload = () => {
                         </div>
                     )}
                     <hr className="border-b-1 border-black" />
+                    </div>
+                   
                 </div>
-            </div>
+            
         </>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Template from "@/components/Template";
 import { useStepStore } from "@/store/stepStore";
 import { useTemplateStore } from "@/store/templateStore";
 import Signature from "@/components/Signature";
+import clsx from "clsx";
 import Upload from "@/components/Upload";
 
 export default function Home() {
@@ -24,7 +25,7 @@ export default function Home() {
         <Header />
       </div>
 
-      <div className="flex w-full py-10 bg-background">
+      <div className="flex w-full py-[16px] bg-background">
         <aside className="sticky hidden basis-2/12 h-screen top-8 lg:block bg-window border-x-2 border-gray-400">
           {/* Left column area */}
           <div className="grid justify-items-end">
@@ -32,33 +33,27 @@ export default function Home() {
           </div>
         </aside>
 
-        {step === 0
-        ?
-        <main className=" basis-10/12 bg-window overflow-y-auto max-h-screen">
-          {step === 0 && <Upload />}
+        <main
+          className={clsx(
+            " basis-4/12 bg-window overflow-y-auto max-h-screen",
+            {
+              "basis-10/12": step === 0,
+            }
+          )}
+        >
           {step === 1 && <Template />}
           {step === 2 && <Info />}
           {step === 3 && <Customization />}
+          {step === 0 &&  <Upload />}
         </main>
-        :
-        <main className=" basis-4/12 bg-window overflow-y-auto max-h-screen">
-          {step === 0 && <Info />}
-          {step === 1 && <Template />}
-          {step === 2 && <Info />}
-          {step === 3 && <Customization />}
-        </main>
-        }
 
-        {step === 0
-        ?
-        null
-        :
-        <aside className="sticky hidden basis-6/12 h-screen top-8 p-5 xl:block bg-window border-x-2 border-gray-400">
-          <div className="p-10">
-            <Preview />
-          </div>
-        </aside>
-        }
+        {step != 0 && (
+          <aside className="sticky hidden basis-6/12 h-screen top-8 p-5 xl:block bg-window border-x-2 border-gray-400">
+            <div className="p-10">
+              <Preview />
+            </div>
+          </aside>
+        )}
       </div>
     </>
   );
