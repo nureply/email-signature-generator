037cbb161fc4248a1fed4ff901c0a2797ac32ea8
ozan isgor <ozanisgor11@gmail.com>
2023-09-22 14:13:17 +0300

    UPDATE: font to Inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
+      <div className={`${inter.className}`}>
+        <Header />
 
-      <Component {...pageProps} />
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
