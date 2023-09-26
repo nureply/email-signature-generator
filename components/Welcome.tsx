66b6833c92a90a6bacd31e4629d41bdc1470d7f5
ozan isgor <ozanisgor11@gmail.com>
2023-09-26 14:32:26 +0300

    UPDATE: welcome

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -24,6 +24,23 @@ const Welcome = () => {
           </p>
         </div>
 
+        <div className="max-w-3xl mx-auto mb-20 mt-10">
+          <p className="text-base sm:text-lg text-center text-gray-900">
+            Looking for the best free email signature creator on the web? Look
+            no further. Our Email Signature Generator is designed to give you
+            the most professional email signature you can imagine. From adding
+            custom logos to sprucing up your sign-off with unique images,
+            we&apos;ve got you covered. Seriously, this is the best way to
+            create an email signature that will wow your contacts. Want to make
+            a free email signature that stands out? Our platform allows you to
+            create an email signature with a logo and even add images for that
+            extra flair. If you&apos;re a Gmail user, you&apos;ll be thrilled to
+            know you can easily create an HTML signature that seamlessly
+            integrates with your account. It&apos;s not just a signature;
+            it&apos;s your digital identity.
+          </p>
+        </div>
+
         <div className="max-w-3xl min-h-screen mx-auto bg-window">
           <p className="p-2 text-center text-lg sm:text-2xl font-semibold text-gray-950">
             How to create an email signature?
@@ -35,10 +52,15 @@ const Welcome = () => {
                 <div className="flex gap-x-5">
                   <div>
                     <h3 className="md:text-lg font-semibold text-gray-900 ">
-                      Choose a Template
+                      Why is an email signature important?
                     </h3>
                     <p className="mt-1 text-gray-800">
-                      Choose a template and start filling in your information
+                      An email signature is much more than a simple sign-off;
+                      it&apos;s your digital handshake. It lends credibility to
+                      your correspondence and gives recipients an easy way to
+                      contact you or follow you on social media. Think of it as
+                      your mini-resume at the end of every email—professional,
+                      yet accessible.
                     </p>
                   </div>
                 </div>
@@ -48,10 +70,15 @@ const Welcome = () => {
                 <div className="flex gap-x-5">
                   <div>
                     <h3 className="md:text-lg font-semibold text-gray-900 ">
-                      Customize the Style
+                      What is a professional email signature?
                     </h3>
                     <p className="mt-1 text-gray-800">
-                      Pick the font, colors, and adjust the size
+                      A professional email signature is a well-crafted sign-off
+                      that typically includes your name, job title, company, and
+                      contact details. But that&apos;s just the tip of the
+                      iceberg! The best professional signatures also include
+                      social media buttons, a profile picture or logo, and even
+                      a catchy tagline or call-to-action.
                     </p>
                   </div>
                 </div>
@@ -61,12 +88,32 @@ const Welcome = () => {
                 <div className="flex gap-x-5">
                   <div>
                     <h3 className="md:text-lg font-semibold text-gray-900 ">
-                      Settle on the Design
+                      What should an email signature include?
                     </h3>
-                    <p className="mt-1 text-gray-800">
-                      Create a professional email signature that enhances your
-                      image and does not impact your email deliverability
-                    </p>
+                    <div className="mb-4">
+                      <p className="mt-1 text-gray-800">
+                        At a minimum, your email signature should include:
+                      </p>
+                      <ul className="list-disc ml-5 mt-2">
+                        <li>Your full name</li>
+                        <li>Job title</li>
+                        <li>Company name</li>
+                        <li>Contact details (email and phone number)</li>
+                      </ul>
+                    </div>
+                    <div>
+                      <p className="mt-1 text-gray-800">
+                        For extra oomph, consider adding:
+                      </p>
+                      <ul className="list-disc ml-5 mt-2">
+                        <li>Social media buttons</li>
+                        <li>A profile picture or company logo</li>
+                        <li>
+                          Any certifications or awards you&apos;ve received
+                        </li>
+                        <li>A brief tagline or call-to-action</li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </li>
@@ -75,23 +122,15 @@ const Welcome = () => {
                 <div className="flex gap-x-5">
                   <div>
                     <h3 className="md:text-lg font-semibold text-gray-900 ">
-                      Integrate with Your Email
+                      Should you put your email in your email signature?
                     </h3>
                     <p className="mt-1 text-gray-800">
-                      Copy-paste your finished signature to your preferred email
-                      clients
+                      While it might seem redundant, including your email in
+                      your signature is actually a smart move. Why? Because it
+                      makes it super easy for people to copy and paste your
+                      contact details directly. It&apos;s all about making life
+                      easier for the folks you&apos;re communicating with!
                     </p>
-                  </div>
-                </div>
-              </li>
-
-              <li className="py-8 first:pt-0 last:pb-0 text-gray-800">
-                <div className="flex gap-x-5">
-                  <div>
-                    <h3 className="md:text-lg font-semibold text-gray-900 ">
-                      Have Your Email Signature Ready?
-                    </h3>
-                    <p className="mt-1 text-gray-800">Excellent!</p>
                   </div>
                 </div>
               </li>
